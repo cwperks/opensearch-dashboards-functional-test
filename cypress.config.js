8e884c6643cb3218d52ffa8b23b27f49c4483ffa
Craig Perkins <cwperx@amazon.com>
2023-08-23 09:09:31 -0400

    Optimize memory
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- cypress.config.js
+++ cypress.config.js
@@ -34,5 +34,7 @@ module.exports = defineConfig({
     },
     baseUrl: 'http://localhost:5601',
     specPattern: '**/*.(js|ts|tsx)',
+    numTestsKeptInMemory: 5,
+    experimentalMemoryManagement: true,
   },
 });
